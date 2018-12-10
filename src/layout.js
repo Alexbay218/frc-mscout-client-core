3ed6116e4797369bfec22367efebb1ec1d98f9e8
Shaun Wu <Alexbay218@gmail.com>
2018-12-09 22:45:51 -0500

    Complete init screen and qrcode screen

--- src/layout.js
+++ src/layout.js
@@ -1,16 +1,56 @@
 var layoutClass = {
   dispElement:{},
   eventObj:{},
+  initLayoutObj: {},
+  dataProtocolObj: {},
   initFunct: function(events) {
     this.dispElement = document.getElementById("display");
-    this.eventObj = events;
-    this.layoutFunct();
     this.dispElement.style.width = window.innerWidth;
     this.dispElement.style.height = window.innerHeight;
+	
+    this.eventObj = events;
+	this.dataProtocolObj = Object.assign({}, dataProtocolClass);
+	this.initLayoutObj = Object.assign({}, initLayoutClass);
+	
+	this.initLayoutObj.contFunct = () => {
+		this.layoutFunct();
+		this.eventObj.assignEventListeners();
+	};
+	this.initLayoutObj.backFunct = () => {
+		if(this.dataProtocolObj.hasDisplayed) {
+			this.dataProtocolObj.initFunct();
+			this.dataProtocolObj.displayFunct();
+		}
+	};
+	
+	this.dataProtocolObj.backFunct = () => {
+		this.resetFunct();
+	}
+	
+	this.initLayoutObj.initFunct();
+  },
+  resetFunct: function() {
+	this.dataProtocolObj.isShowing = false;
+	this.eventObj.resetTimeFunct();
+	this.initLayoutObj.initFunct();
   },
   layoutFunct: function() {
+	this.dispElement.innerHTML = "";
     var groupSum =  this.eventObj.eventJson.variableHeightWeight;
+    //Variables
     this.dispElement.insertAdjacentHTML("beforeend", "<div class=\"group\" id=\"variables\"></div>");
+    var tmp = document.getElementById("variables");
+    tmp.insertAdjacentHTML("beforeend","<div class=\"variable\" id=\"time\"></div>");
+    var tmp2 = document.getElementById("time");
+    tmp2.style.height = "100%";
+    tmp2.style.width = ((1/(this.eventObj.eventJson.variables.length+1))*100).toString() + "%";
+    for(var i = 0;i < this.eventObj.eventJson.variables.length;i++) {
+      tmp.insertAdjacentHTML("beforeend","<div class=\"variable\" id=\"" + this.eventObj.eventJson.variables[i].variableName + "\"></div>");
+      tmp2 = document.getElementById(this.eventObj.eventJson.variables[i].variableName);
+      tmp2.style.height = "100%";
+      tmp2.style.width = ((1/(this.eventObj.eventJson.variables.length+1))*100).toString() + "%";
+    }
+    //Groups
     for(var i = 0;i < this.eventObj.eventJson.layouts.length;i++) {
       var hasCurr = false;
       for(var j = 0;j < i;j++) {
@@ -23,7 +63,8 @@ var layoutClass = {
         groupSum += this.eventObj.eventJson.layouts[i].heightWeight;
       }
     }
-    var tmp = document.getElementById("variables");
+    //Input
+    tmp = document.getElementById("variables");
     tmp.style.width = "100%";
     tmp.style.height = ((this.eventObj.eventJson.variableHeightWeight/groupSum)*100).toString() + "%";
     for(var i = 0;i < this.eventObj.eventJson.layouts.length;i++) {
@@ -43,5 +84,32 @@ var layoutClass = {
       tmp.style.height = "100%";
       tmp.style.width = ((this.eventObj.eventJson.layouts[i].widthWeight/groupSum)*100).toString() + "%";
     }
+    this.updateFunct();
+  },
+  updateFunct: function() {
+    if(this.eventObj.isGoing) {
+      this.eventObj.updateTimeFunct();
+    }
+    //Variables
+    var tmp = document.getElementById("time");
+	if(tmp != null) {
+		tmp.innerText = "Time: " + this.eventObj.timeDelta.toFixed(2);
+		for(var i = 0;i < this.eventObj.eventJson.variables.length;i++) {
+		  tmp = document.getElementById(this.eventObj.eventJson.variables[i].variableName);
+		  tmp.innerText = this.eventObj.eventJson.variables[i].variableTitle + ": " + this.eventObj.eventJson.variables[i].variableAmount;
+		}
+		//input
+		for(var i = 0;i < this.eventObj.eventJson.events.length;i++) {
+		  tmp = document.getElementById(this.eventObj.eventJson.events[i].eventName);
+		  tmp.innerText = this.eventObj.eventJson.events[i].eventTitle;
+		}
+	}
+	//Pass data to qrcode
+	if(this.eventObj.timeDelta >= 3) {
+		this.dataProtocolObj.inputFunct(Object.assign({}, this.initLayoutObj.metadataObj), this.eventObj.initEventVars.slice(), this.eventObj.eventLog.slice());
+		this.dataProtocolObj.initFunct();
+		this.dataProtocolObj.displayFunct();
+		this.eventObj.resetTimeFunct();
+	}
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
-var lyt, evt;
-window.addEventListener('message', function(event) {
-  lyt = Object.assign({}, layoutClass);
-  evt = Object.assign({}, eventsClass);
-  evt.initFunct(event.data);
-  lyt.initFunct(evt);
-  evt.assignEventListeners();
-});
+
+var lyt = Object.assign({}, layoutClass);
+var evt = Object.assign({}, eventsClass);
+var dispElement = document.getElementById("display");
+evt.initFunct(JSON.stringify(eventConfig));
+lyt.initFunct(evt);
+
+window.setInterval(() => {
+    dispElement.style.width = window.innerWidth;
+    dispElement.style.height = window.innerHeight;
+	lyt.updateFunct();
+},10)
 
 /*
-var qrcs = Object.assign({}, qrcodestreamerClass);
+var qrcsObj = Object.assign({}, qrcodestreamerClass);
 var disp = document.getElementById("display");
-var hashdisp = document.getElementById("hash");
+//var hashdisp = document.getElementById("hash");
 disp.width = 200;
 disp.height = 200;
-var qr = new QRCode(disp, {width: disp.width, height: disp.height, correctLevel: QRCode.CorrectLevel.L});
+var qr = new QRCode("display", {width: disp.width, height: disp.height, correctLevel: QRCode.CorrectLevel.L});
 var t = 500;
-qrcs.initFunct(qr,ADLER32);
-qrcs.inputData("This is a test of the qrcode streaming using long amounts of data. 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789\nThis is a test of the qrcode streaming using long amounts of data. 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789");
-hashdisp.innerText = qrcs.fullHash + " " + t;
+qrcsObj.initFunct(qr,ADLER32);
+qrcsObj.inputData("This is a test of the qrcode streaming using long amounts of data. 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789\nThis is a test of the qrcode streaming using long amounts of data. 123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789");
+//hashdisp.innerText = qrcs.fullHash + " " + t;
 var funct = () => {
-  qrcs.nextCode();
-  t = document.getElementById("timing").value;
-  hashdisp.innerText = qrcs.fullHash + " " + t;
+  qrcsObj.nextCode();
+  //t = document.getElementById("timing").value;
+  //hashdisp.innerText = qrcsObj.fullHash + " " + t;
   window.setTimeout(funct, t);
 }
 funct();
